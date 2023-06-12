346b1c384151829d700ac18cd68d06f4a41e08a6
Iqbal-Baloch <iqbalsyedmuhammad1@gmail.com>
2023-06-12 16:29:25 +0500

    write html and css for application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
+const handleAddTodo = (e)=> {
+  e.preventDefault() ;
+}
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <div className='App'>
+    <h1>Todo Application</h1>
+    <div className='todoApp'>
+      <form onSubmit={handleAddTodo}>
+        <input type='submit' value="Add"/>
+        <input type='text' required/>
+      </form>
+      <div className='todoList'>
+          <ul className='todoListItem'>
+            <li className='item-checkbox'>
+              <input type='checkbox' name='item-checkbox'/>
+              todo message here we goes....
+            </li>
+            <li className='item-delete'>
+            <button>dellete</button>
+            <button>update</button>
+            </li>  
+          </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+   </div>
+  ); 
 }
 
 export default App
